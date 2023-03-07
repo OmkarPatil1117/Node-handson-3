8ef565518da5ecb092f30a873c2a5dd7439bd5b2
OmkarPatil1117 <omkrpatil117@gmail.com>
2023-03-07 11:35:03 +0530

    Commit

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-const express = require("express");
+const express = require('express')
+const cors = require('cors')
+const {calculation,subs} = require("./controller/calculations")
 
-const app = express();
 
-const middleware = (req,res,next)=>{
-    console.log("Midddleware");
-    console.warn("current route is", req.originalUrl);
-    next();
+const app = express()
+// const Whitelist = ["*"]
+app.use(cors(
+    // {
+    //     origin: Whitelist,
+    //     methods : ['GET', 'POST']
+    // }
+))
+
+const middleware = (req, res, next) =>{
+    let result = 5 ;
+    if(result === calculation(2,3)){
+        console.log('middleware calling!!');
+        next()
+    }
+    else{
+        res.status(400).send('You are Not Allowed')
+    }
 }
 
-app.get('/', (req,res)=>{
-    res.send('This is my HomePage')
+const middleware1 = (req, res, next) =>{
+    console.log('I am on home page');
+    next()
+}
+
+app.get('/',cors(), middleware, (req, res)=>{
+    res.send('You Are Logged In')
 })
 
-app.get('/about', middleware,(req,res)=>{
-    console.log('This is About');
-    res.send("This is My About Page")
+app.get('/home', middleware1, (req,res)=>{
+    res.send('You are at the Home Page')
 })
 
-app.listen(8000, ()=>{
-    console.log("Started")
+app.post('/home', middleware1, (req,res) =>{
+    res.send(JSON.stringify([
+        {
+            name : 'Omkar',
+            age : 21,
+            batch : 'EA17',
+            Designation : 'Full Stack Web Developer'
+        }
+        
+    ]))
+})
+
+app.listen(3001, ()=>{
+    console.log("server is running !!");
 })
